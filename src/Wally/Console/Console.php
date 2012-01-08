999ee3c002fcccb79d9a00dc7dcf442de132ccba
Walter Dal Mut <walter.dalmut@gmail.com>
2012-01-08 15:52:35 +0100

    Base console colors

---
+++ src/Wally/Console/Console.php
@@ -0,0 +1,67 @@
+<?php
+namespace Wally\Console;
+ 
+class Console
+{
+    private static $_instance;
+    
+    const GREEN = "green";
+    const RED = "red";
+    const YELLOW = "yellow";
+    
+    private $_colors = array(
+        self::GREEN => array("\033[32m", "\033[37m"),
+        self::RED => array("\033[31m", "\033[37m"),
+        self::YELLOW => array("\033[33m", "\033[37m")
+    );
+    
+    private function __construct() {}
+    private function __clone(){throw new \Exception("not supported");}
+    
+    private $_prefix;
+    private $_postfix;
+    
+    public static function getInstance()
+    {
+        if (!self::$_instance) {
+            self::$_instance = new self();
+            
+            list(self::$_instance->_prefix, self::$_instance->_postfix) = self::$_instance->_getColor(self::GREEN);
+        }
+        
+        return self::$_instance;
+    }
+    
+    private function _getColor($color)
+    {
+        return $this->_colors[$color];
+    }
+    
+    public function setColor($color = self::GREEN) {
+        list($this->_prefix, $this->_postfix) = self::$_instance->_getColor($color);
+    }
+    
+    private function _isPosix()
+    {
+        return (function_exists('posix_isatty'));
+    }
+    
+    public function _get($message)
+    {
+        if ($this->_isPosix()) {
+            return $this->_prefix . $message . $this->_postfix;
+        } else {
+            return $message;
+        }
+    }
+    
+    public function sprintf()
+    {
+        $args = func_get_args();
+
+        $message = array_shift($args);
+        $message = vsprintf($message, $args);
+        
+        return $this->_get($message);
+    }
+}
